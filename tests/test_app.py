d87bf8e80a5774b08f4740a00938dc2e9a558e8d
arturj <arturjardel9@gmail.com>
2023-09-10 17:01:10 -0300

    Configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+from taskload.app import app
+
+
+def test_root_deve_retornar_200_e_ola_mundo():
+
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
